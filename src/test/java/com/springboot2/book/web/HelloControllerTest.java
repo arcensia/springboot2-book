92d5836cb226d38f129ff9b45f6fd459ab5d1fc4
kim-yeonghyeon <zxcklwe@naver.com>
2023-12-11 19:28:52 +0900

    add hellowController

---
+++ src/test/java/com/springboot2/book/web/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.springboot2.book.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(statues().isOk())
+                .andExpect(content().string(hello));
+    }
+
+}

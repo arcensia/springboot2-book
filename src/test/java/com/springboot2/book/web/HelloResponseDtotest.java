88a74363a65f24525638303320b7d82dc4e45576
kim-yeonghyeon <zxcklwe@naver.com>
2023-12-13 23:59:17 +0900

    miss

--- src/test/java/com/springboot2/book/web/HelloResponseDtotest.java
+++ src/test/java/com/springboot2/book/web/HelloResponseDtotest.java
@@ -1,19 +1,19 @@
 package com.springboot2.book.web;
 
 import com.springboot2.book.web.dto.HelloResponseDto;
-import org.junit.jupiter.api.Test;
-
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 public class HelloResponseDtotest {
     @Test
     public void lombok_test(){
         String name = "test";
         int amount = 1000;
+        HelloResponseDto dto= new HelloResponseDto(name, amount)
+        Assertions.assertThat(dto.name()).isEqualTo(name);
+        Assertions.assertThat(dto.amount()).isEqualTo(amount);
 
-        HelloResponseDto dto = new HelloResponseDto(name, amount);
-
-        Assertions.assertThat(dto.getName().isEqualTo(name));
-        Assertions.assertThat(dto.getAmount().isEqualTo(amount));
+//        원본소스
+//        HelloResponseDto dto = new HelloResponseDto(name, amount);
 //        assertThat(dto.getName().isEqualTo(name));
 //        assertThat(dto.getAmount().isEqualTo(amount));
     }

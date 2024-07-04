792b28c6f49cda5ba38f846d3a97d3125775548e
kim-yeonghyeon <zxcklwe@naver.com>
2024-07-04 22:29:15 +0900

    update가 안되던 문제해결. 현상: update기능이 동작하지 않음.
    원인: service/PostsService의 save, update 메소드가 @Transient로 작성되어있었음
    무분별한 자동완성기능을 사용하여 발생함.
    
    해결: @Transient→ @Transactional 변경.

--- src/main/java/com/springboot2/book/service/PostsService.java
+++ src/main/java/com/springboot2/book/service/PostsService.java
@@ -8,22 +8,20 @@ import com.springboot2.book.web.dto.PostsSaveRequestDto;
 import com.springboot2.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.beans.Transient;
 
 @RequiredArgsConstructor
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
 
-    @Transient
+    @Transactional
     public Long save(PostsSaveRequestDto requestDto){
-
         return postsRepository.save(requestDto.toEntity()).getId();
-
     }
 
-    @Transient
+    @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id: " + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());

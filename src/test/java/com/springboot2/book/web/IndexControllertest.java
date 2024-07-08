9c8b769b49d5936bf415be5b2ae2cde70562eaaf
kim-yeonghyeon <zxcklwe@naver.com>
2024-07-08 23:59:21 +0900

    153p 4.2 기본페이지 만들기 실습

--- src/test/java/com/springboot2/book/web/IndexControllertest.java
+++ src/test/java/com/springboot2/book/web/IndexControllertest.java
@@ -1,7 +1,23 @@
 package com.springboot2.book.web;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IndexControllertest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 페이지_로딩(){
+        //when
+        String body = restTemplate.getForObject("/", String.class);
+
+        //then
+        Assertions.assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
 }

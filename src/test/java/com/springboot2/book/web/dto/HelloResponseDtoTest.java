77a6bee6f0840939a4051603c208b596c5a67315
Kim <zxcklwe@naver.com>
2026-03-19 18:08:11 +0900

    180p OAuth2 추가

---
+++ src/test/java/com/springboot2/book/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,16 @@
+package com.springboot2.book.web.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloResponseDtoTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getAmount() {
+    }
+}

720dd5a9971f822440ad4711ba2eaeaa2a6c48e5
Kim <zxcklwe@naver.com>
2026-03-28 22:44:54 +0900

    feat: OAuth2 user service 추가 (188p)

--- src/main/java/com/springboot2/book/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/springboot2/book/config/auth/CustomOAuth2UserService.java
@@ -1,13 +1,20 @@
 package com.springboot2.book.config.auth;
 
+import com.springboot2.book.config.auth.dto.OAuthAttributes;
+import com.springboot2.book.config.auth.dto.SessionUser;
+import com.springboot2.book.domain.user.User;
 import com.springboot2.book.domain.user.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
@@ -20,5 +27,24 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
             throws OAuth2AuthenticationException {
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest
+                .getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        User user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(
+                Collections.singleton(
+                        new SimpleGrantedAuthority(user.getRoleKey()),
+                        attributes.getAttributes(),
+                        attributes.getNameAttributeKey())
+                );
+
     }
 }

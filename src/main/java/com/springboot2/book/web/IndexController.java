da04fd964cdf8feccc208898f0118e1518512d73
Kim <zxcklwe@naver.com>
2026-03-30 20:10:00 +0900

    feat: annotation 등록 추가 (200p)

--- src/main/java/com/springboot2/book/web/IndexController.java
+++ src/main/java/com/springboot2/book/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.springboot2.book.web;
 
+import com.springboot2.book.config.auth.LoginUser;
 import com.springboot2.book.config.auth.dto.SessionUser;
 import com.springboot2.book.service.PostsService;
 import com.springboot2.book.web.dto.PostsResponseDto;
@@ -18,9 +19,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null){
             model.addAttribute("userName", user.getName());
         }
